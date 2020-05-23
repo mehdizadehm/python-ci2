7a3336d315297e0c820632f09f233c6f082c9096
mehdizadehm <53583197+mehdizadehm@users.noreply.github.com>
2020-05-23 18:50:48 +0200

    Update test_sample1.py

--- tests/test_sample1.py
+++ tests/test_sample1.py
@@ -1,24 +1,34 @@
-import pytest
-
-def test_file1_method1():
-    x=2
-    x=5
-    y=6
-    # assert x+1 == y,"test failed"
-    assert x == y,"test failed"
+import unittest
 
 
-def test_file1_method2():
-    x=5
-    y=6
-    assert x+1 == y,"test failed without any reason! :)) "
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+    def test_file1_method1(self):
+        x = 2
+        x = 6
+        y = 6
+        # assert x+1 == y,"test failed"
+        assert x == y, "test failed"
+
+    def test_file1_method2(self):
+        x = 5
+        y = 6
+        assert x + 1 == y, "test failed without any reason! :)) "
 
 
-def func(x):
-    return x + 1
-
-
-def test_answer():
-    echo "Test started.."
-    assert func(4) == 5, "answer test failed"
-    
+if __name__ == '__main__':
+    unittest.main()
