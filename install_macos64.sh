343f599abc82b15ca1a8df62820bdad720a1182b
mehdizadehm <53583197+mehdizadehm@users.noreply.github.com>
2020-07-26 16:08:05 +0200

    add installer

--- install_macos64.sh
+++ install_macos64.sh
@@ -1,7 +1,48 @@
-echo "Installing MintPy ..."
+#!/bin/sh
+LOG_FILE="log.txt"
+OS=MacOS64
+MINICONDA_FILENAME=Miniconda3-latest-MacOSX-x86_64.sh
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+> ${LOG_FILE} 
+exec 3>&1 1>>${LOG_FILE} 2>&1
+
+process_start()
+{
+    echo  | tee /dev/fd/3 
+    echo "MintPy" | tee /dev/fd/3   
+    echo "The Miami INsar Time-series software in PYthon" | tee /dev/fd/3  
+    echo "Version 1.2.3" | tee /dev/fd/3      
+    echo  | tee /dev/fd/3 
+    echo "Installation starting..." | tee /dev/fd/3
+    echo "Operating System: ${OS}" | tee /dev/fd/3
+    echo "You may follow the installtion in log.txt" 1>&3
+}
+
+process_exit()
+{
+    echo  | tee /dev/fd/3 
+    echo "${RED}ERROR${NC}" 1>&3
+    echo "Read the log file for more details, log.txt" 1>&3
+    echo "You may use the README to install MintPy manually." | tee /dev/fd/3
+    echo "If MintPy is already installed on your machine, please uninstall/remove it before installing the new version." | tee /dev/fd/3
+    echo  | tee /dev/fd/3 
+    exit 1
+}
+
+process_completed()
+{
+    echo  | tee /dev/fd/3 
+    echo "MintPy is installed successfully!" | tee /dev/fd/3
+    echo "For more detials about installtion steps please read the log.txt" 1>&3
+    echo  | tee /dev/fd/3 
+}
+
+process_start
 
 if [ -z ${PYTHONPATH+x} ]; then export PYTHONPATH=""; fi
 
+echo "Setting environment variables..." | tee /dev/fd/3
 ##--------- MintPy ------------------##
 export MINTPY_HOME=~/tools/MintPy
 export PYTHONPATH=${PYTHONPATH}:${MINTPY_HOME}
@@ -18,62 +59,94 @@ apt update
 apt-get update --yes && apt-get upgrade --yes
 # apt-get install --yes git wget
 
+
 if ! command -v git &> /dev/null
 then
+    echo "Installing git..." | tee /dev/fd/3
     apt-get install --yes git wget
 fi
 
 if ! command -v wget &> /dev/null
 then
+    echo "Installing wget..." | tee /dev/fd/3
     apt-get install --yes git wget
 fi
 
 # download source code MintPy and 
-git clone https://github.com/insarlab/MintPy.git $MINTPY_HOME
+{ # try
+    echo  | tee /dev/fd/3 
+    echo "Downloading MintPy source code..." | tee /dev/fd/3
+    echo "Destination: ${MINTPY_HOME}" | tee /dev/fd/3
+    git clone https://github.com/insarlab/MintPy.git $MINTPY_HOME
+    #save your output
+    echo "Mintpy downloaded!" | tee /dev/fd/3
+
+} || { # catch
+    # save log for exception 
+    echo "${RED}Could not download Mintpy!${NC}" | tee /dev/fd/3
+    process_exit
+}
+
 
 # download and install miniconda3
 mkdir -p ${HOME}/tools
 cd ${HOME}/tools
 
-
+# echo  | tee /dev/fd/3 
 { # try
-
-    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh &&
+    echo "Downloading Miniconda for ${OS} ..." | tee /dev/fd/3
+    wget https://repo.anaconda.com/miniconda/${MINICONDA_FILENAME} &&
     #save your output
-    echo "Miniconda Installer downloaded!"
+    echo "Miniconda Installer downloaded!" | tee /dev/fd/3
 
 } || { # catch
     # save log for exception 
-    echo "Could not download Miniconda!"
-    exit 1
+    echo "${RED}Could not download Miniconda!${NC}" | tee /dev/fd/3
+    process_exit
 }
 
-
-
+echo  | tee /dev/fd/3 
 { # try
-
-    chmod +x Miniconda3-latest-MacOSX-x86_64.sh &&
-    ./Miniconda3-latest-MacOSX-x86_64.sh -b -p ${HOME}/tools/miniconda3 &&
+    echo "Installing Miniconda ..." | tee /dev/fd/3
+    echo "Destination: ${CONDA_PREFIX}" | tee /dev/fd/3
+    echo "The installation process may take several minutes."  | tee /dev/fd/3
+    chmod +x ${MINICONDA_FILENAME} &&
+    ./${MINICONDA_FILENAME} -b -p ${HOME}/tools/miniconda3 &&
     ~/tools/miniconda3/bin/conda init bash &&
     #save your output
-    echo "Miniconda installed successfully!"
+    echo "Miniconda installed successfully." | tee /dev/fd/3
 
 } || { # catch
     # save log for exception 
-    echo "Miniconda Installation failed"
-    exit 1
+    echo "${RED}Miniconda Installation failed!" | tee /dev/fd/3
+    process_exit
 }
 
 # setup environment variable
 export PYTHONPATH=${PYTHONPATH}:${MINTPY_HOME}:${PYAPS_HOME}
 export PATH=${MINTPY_HOME}/mintpy:${CONDA_PREFIX}/bin:${PATH}
-# download source code PyAPS
-git clone https://github.com/yunjunz/pyaps3.git $PYAPS_HOME/pyaps3
 
+# download source code PyAPS  
+{ # try
+    echo  | tee /dev/fd/3 
+    echo "Downloading PyAPS source code..." | tee /dev/fd/3
+    echo "Destination: ${PYAPS_HOME}" | tee /dev/fd/3
+    git clone https://github.com/yunjunz/pyaps3.git $PYAPS_HOME/pyaps3
+    #save your output
+    echo "PyAPS downloaded!" | tee /dev/fd/3
+
+} || { # catch
+    # save log for exception 
+    echo "${RED}Could not download PyAPS!${NC}" | tee /dev/fd/3
+    process_exit
+}
+
+echo  | tee /dev/fd/3 
 # install dependencies
 { # try
-
-    echo "installing dependencies..." &&
+    echo "Installing dependencies..." | tee /dev/fd/3
+    echo "The installation process may take several minutes."  | tee /dev/fd/3
+    chmod +x ${MINICONDA_FILENAME} &&
     conda config --add channels conda-forge &&
     conda install --yes --file $MINTPY_HOME/docs/conda.txt &&
     pip install git+https://github.com/tylere/pykml.git &&
@@ -84,28 +157,31 @@ git clone https://github.com/yunjunz/pyaps3.git $PYAPS_HOME/pyaps3
     # $CONDA_PREFIX/bin/pip install git+https://github.com/tylere/pykml.git
 
     #save your output
-    echo "dependencies could not be installed!"
+    echo "Dependencies installed successfully." | tee /dev/fd/3
 
 } || { # catch
     # save log for exception 
-    echo "Dependencies installed successfully!"
+    echo "${RED}Dependencies couldn't be installed!" | tee /dev/fd/3
+    process_exit
 }
 
 
-
-test installation
+echo  | tee /dev/fd/3 
+# test installation
 { # try
-
+    echo "Testing Installation..." | tee /dev/fd/3
     python -c "import mintpy; print(mintpy.version.description)" &&
     smallbaselineApp.py -h &&
     tropo_pyaps3.py -h
+
+    echo "Installation test was successfull."  | tee /dev/fd/3    
     
     #save your output
-    echo "MitPy was installed successfully!"
+    process_completed
 
 } || { # catch
     # save log for exception 
-    echo "Installation could not be completed!"
-    echo "Python test failed!"
-    exit 1
+    echo "Installation could not be completed."  | tee /dev/fd/3
+    echo "Python test failed!"  | tee /dev/fd/3
+    process_exit
 }
